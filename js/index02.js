4234aa494e792bd790147a45d547412d5f24657d
JRE-Sun <jresun@163.com>
2016-12-24 13:34:59 +0800

    index02第三个轮播(没修复)

--- js/index02.js
+++ js/index02.js
@@ -3,37 +3,81 @@ window.onload = function() {
     var content_img = document.querySelectorAll(".content-img");
     var arrSpan = document.querySelectorAll(".scroll-div span");
     var mask = document.querySelector(".mask");
+    var pre = document.querySelector(".pre");
+    var next = document.querySelector(".next");
+    var scroll_nav_ul = document.querySelector(".scroll-nav-ul");
     // alert(content_img.length);
+
+    var a = [];
 
     for (var x = 0; x < scroll_nav_img.length; x++) {
         scroll_nav_img[x].index = x;
-        scroll_nav_img[x].addEventListener("mouseover", function() {
-            for (var r = 0; r < scroll_nav_img.length; r++) {
-                removeClass(scroll_nav_img[r], "scroll-nav-img-active");
-                removeClass(content_img[r], "content-img-active");
-                removeClass(arrSpan[r], "span-active");
-                removeClass(arrSpan[r], "span-active");
-
-                // removeClass(arrP[p], "p-active");
-            }
-            addClass(scroll_nav_img[this.index], "scroll-nav-img-active");
-            addClass(content_img[this.index], "content-img-active");
-            addClass(arrSpan[this.index], "span-active");
-            addClass(mask, "mask-change");
-
-            // addClass(arrP[this.index], "p-active");
-            // p = n = this.index;
-        }, false);
+        a[x] = scroll_nav_img[x].addEventListener("mouseover", b, false);
     }
-    // for (var x = 0; x < scroll_nav_img.length; x++) {
-    //     scroll_nav_img[x].addEventListener("mouseout", function() {
 
-    //         removeClass(mask, "mask-change");
+    var timer = setInterval(b, 3000);
+    var p = 0;
 
-    //         // addClass(arrP[this.index], "p-active");
-    //         // p = n = this.index;
-    //     }, false);
-    // }
+    function b() {
+        for (var r = 0; r < scroll_nav_img.length; r++) {
+            removeClass(scroll_nav_img[r], "scroll-nav-img-active");
+            removeClass(content_img[r], "content-img-active");
+            removeClass(arrSpan[r], "span-active");
+            removeClass(arrSpan[r], "span-active");
+
+            // removeClass(arrP[p], "p-active");
+        }
+        // this.getAttribute("uid");
+        addClass(scroll_nav_img[p], "scroll-nav-img-active");
+        addClass(content_img[p], "content-img-active");
+        addClass(arrSpan[p], "span-active");
+        // addClass(mask, "mask-change");
+
+        // addClass(arrP[this.index], "p-active");
+        // p = n = this.index;
+        preMove();
+        p++;
+    }
+    b();
+    var scroll_nav_img_width = getDefaultStyle(scroll_nav_img[0], "width").substring(0, getDefaultStyle(scroll_nav_img[0], "width").indexOf("p"));
+
+
+
+    pre.addEventListener("click", function() {
+        preMove();
+    }, false);
+
+    function preMove() {
+        var cscroll_nav_ul_left = getDefaultStyle(scroll_nav_ul, "left").substring(0, getDefaultStyle(scroll_nav_ul, "left").indexOf("p"));
+        if (cscroll_nav_ul_left == -880) {
+            scroll_nav_ul.style.left = "0px";
+            // alert();
+        } else {
+            scroll_nav_ul.style.left = (cscroll_nav_ul_left - scroll_nav_img_width - 14) + "px";
+        }
+    }
+    next.addEventListener("click", function() {
+        timer = null;
+        p++;
+        if (c == 0) {
+            c = 1;
+            var cscroll_nav_ul_left = getDefaultStyle(scroll_nav_ul, "left").substring(0, getDefaultStyle(scroll_nav_ul, "left").indexOf("p"));
+            if (cscroll_nav_ul_left == 0) {
+                scroll_nav_ul.style.left = "-880px";
+                // alert();
+            } else {
+                scroll_nav_ul.style.left = parseInt(cscroll_nav_ul_left - (-scroll_nav_img_width) - (-14)) + "px";
+                // alert(parseInt(cscroll_nav_ul_left - (-scroll_nav_img_width) - (-14)));
+            }
+            c = 0;
+        }
+        timer = setInterval(b, 3000);
+    }, false);
+
+
+
+
+
 
 
 
